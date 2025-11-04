e1a6efc0c9c65cf43e99b34a4e337084ffb969f7
pratikmulik07 <149190871+pratikmulik07@users.noreply.github.com>
2025-11-04 14:59:14 +0530

    Update Assignment2.java

--- Assignment2.java
+++ Assignment2.java
@@ -1,3 +1,19 @@
+//PRN - 123B1F063
+//Name - Pratik Rajkumar Mulik
+//Date - 
+
+/*
+Movie Recommendation System Optimization 
+A popular OTT platform, StreamFlix, offers personalized recommendations by sorting movies 
+based on user preferences, such as IMDB rating, release year, or watch time popularity. 
+However, during peak hours, sorting large datasets slows down the system. 
+As a backend engineer, you must: 
+● Implement Quicksort to efficiently sort movies based on various user-selected 
+parameters. 
+● Handle large datasets containing of  movies while maintaining fast response times 
+ 
+*/
+
 import java.util.*;
 class Movie {
     String title;
